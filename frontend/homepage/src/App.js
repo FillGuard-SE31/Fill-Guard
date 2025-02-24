bcfb98bf5121ec576e0fe5a8ed808e76075c5e3e
Deyya832 <hashindeyanas@gmail.com>
2025-02-24 19:41:26 +0530

    Created Login Page

--- frontend/homepage/src/App.js
+++ frontend/homepage/src/App.js
@@ -16,6 +16,7 @@ import Team from "./pages/Team";
 import Product from "./pages/Product";
 import Shop from "./pages/Shop";
 import ContactPage from "./pages/ContactUs";
+import Login from "./pages/login";
 
 import "./styles/App.css";
 
@@ -68,6 +69,7 @@ function App() {
           <Route path="/product" element={<Product />} />
           <Route path="/shop" element={<Shop />}/>
           <Route path="/contact-us" element={<ContactPage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </AnimatePresence>
       <Footer />
